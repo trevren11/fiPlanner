d09748fae3d5bb6ad74af072179a746cc2d21b87
Trevor Renshaw <trevren11@gmail.com>
2016-09-24 18:12:44 -0600

    Remove css

--- sass-compile.rb
+++ sass-compile.rb
@@ -1,1 +1,4 @@
-exec 'sass .\core\scss\main.scss .\public\css\main.css'
+#exec 'sass .\core\scss\main.scss .\public\css\main.css'
+
+# Can also run so it auto updates, needs ruby and sass gem installed
+exec 'sass --watch .\core\scss\:public/css'
